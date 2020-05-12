bde9364aa24db909dba7eff7b0a31b919aab02ef
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-12 17:32:25 -0300

    ex3 sobre comparar tres num

---
+++ exercises/4_1/3-maior-num-entre-tres.js
@@ -0,0 +1,16 @@
+//Faça um programa que retorne o maior de três números.
+//Defina no começo do programa três variáveis com os valores que serão comparados.
+
+let a = 5;
+let b = 9;
+let c = 97;
+
+if (a>b && a>c) {
+    console.log(a)
+}
+else if (b>a && b>c) {
+    console.log(b)
+} 
+else {
+    console.log(c)
+};
