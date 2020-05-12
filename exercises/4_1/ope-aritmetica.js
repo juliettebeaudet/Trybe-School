1aae3c8b670847cef0e16fa1bf2e57b31b53bd59
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-12 17:20:52 -0300

    primeiro exercicio sobre operadores aritmeticos

---
+++ exercises/4_1/ope-aritmetica.js
@@ -0,0 +1,18 @@
+//Faça cinco programas, um para cada operação aritmética básica.
+//Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados.
+
+let a = 2;
+let b = 3;
+//Faça programas para:
+//Adição
+let ad = a+b;
+//Subtracão
+let sub = a-b;
+//Multiplicação
+let mult = a*b
+//Divisão
+let div = a/b;
+//Módulo
+let mod = a%b;
+
+console.log(ad, sub, mult, div, mod);
