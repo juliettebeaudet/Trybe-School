7d278388d60ceaca39f4aa993038ace9af361427
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-12 19:45:11 -0300

    ex 11 salario liquido resolvido

--- exercises/4_1/11-salario-liquido.js
+++ exercises/4_1/11-salario-liquido.js
@@ -34,3 +34,28 @@ R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
 
 Resultado: R$ 2.612,55.
 Dica: que tal identificar as alíquotas com variáveis de nomes explicativos? */
+
+let aliquotaINSS;
+let aliquotaIR;
+
+let grossSalary = 2000;
+
+if (grossSalary <= 1556.94) {
+aliquotaINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+aliquotaINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+aliquotaINSS = grossSalary * 0.11;
+} else {
+aliquotaINSS = 570.88;
+}
+
+let baseSalary = grossSalary - aliquotaINSS;
+
+if (baseSalary <= 1903.98) {
+aliquotaIR = 0;
+} else if (baseSalary <= 2826.65) {
+aliquotaIR = baseSalary * 0.075 - 142.80;
+}
+
+console.log("Salário: " + (baseSalary - aliquotaIR)); 
