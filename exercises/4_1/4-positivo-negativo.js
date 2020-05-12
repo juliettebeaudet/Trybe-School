87d9f8281ec0b5f203ef14a379121e55c70a6a5e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-12 17:36:02 -0300

    ex4 sobre detectar se num é positivo, negativo ou igual a zero

---
+++ exercises/4_1/4-positivo-negativo.js
@@ -0,0 +1,15 @@
+//Faça um programa que, dado um valor definido numa variável, 
+//retorne “positive” se esse valor for positivo,
+//“negative” se for negativo e “zero” caso contrário.
+
+let numero = 88;
+
+if (numero > 0) {
+    console.log("positive")
+}
+else if (numero < 0) {
+console.log("negative")
+}
+else {
+    console.log("zero")
+};
