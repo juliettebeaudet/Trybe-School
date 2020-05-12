7ef84e451762453e3a8d8880335f2365749e52a6
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-12 17:26:30 -0300

    Ex 2 completado sobre mostrar o maior num

---
+++ exercises/4_1/2-maior-num.js
@@ -0,0 +1,8 @@
+//Faça um programa que retorne o maior de dois números.
+//Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a = 4;
+let b = 12;
+
+let maiorNumero = (a>b) ? (a) : (b);
+console.log(maiorNumero);
