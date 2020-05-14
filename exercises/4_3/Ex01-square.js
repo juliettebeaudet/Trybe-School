2879f411eaf9e29e277c4475c566689e397d0b82
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-14 19:18:20 -0300

    ex1 created a square of * using two for

--- exercises/4_3/Ex01-square.js
+++ exercises/4_3/Ex01-square.js
@@ -7,21 +7,32 @@ seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n
 *****
 (aqui com n=5 mas pode ser qualquer)
 Desenhando os Baby steps
-1) interpretar: vai ter que ter duplo for 
+1) interpretar: vai ter que ter duplo for.
 2) algoritmo
 Primeiro, tem que ter um for para imprimir a linha com n *
-Segundo, tem que ter um for para dar consolelog nessa linha n vezes
+Segundo, tem que ter um for para imprimir essa linha n vezes, ou seja o 
+console.log tem que ser dentro do segundo for. 
 
-3) codificar o algoritmo como segue:*/
-
-let lineStars='';
+3) codificar o algoritmo como segue:
 let n = 5;
-
-for (let i=0; i<n; i+=1) {
-    lineStars += '*';
-    console.log(lineStars);
-
+for (let i = 0; i < n; i += 1) {
+    let lineStars = '';
+    for (let j = 0; j < n; j += 1) {
+        lineStars += '*';
     }
+    console.log(lineStars);
+}
+Ficou melhor e mais legivel do jeito seguinte: */
+
+let n=5;
+let lineStars = '';
+
+for (let i=0; i<n; i+=1){
+    lineStars += '*';
+}
+for (let j=0;j<n ; j+=1){
+    console.log(lineStars);
+}
 
 
 
