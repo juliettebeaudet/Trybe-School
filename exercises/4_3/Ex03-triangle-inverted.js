f5124c70d0c74acd1f9a3d8112f6dec6d507cf52
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-14 20:43:45 -0300

    Ex3 inverted triangle solved with two triangles

--- exercises/4_3/Ex03-triangle-inverted.js
+++ exercises/4_3/Ex03-triangle-inverted.js
@@ -12,12 +12,25 @@ na segunda, 3esp e 2*
 terceira 2esp e 3*
 quarta 1esp e 4*
 quinta 0esp 5*.
-*/ 
+OU é uma suma de duas piramides:
+uma decrescente com vazios repetida 4vezes, uma de estrelas como no exercicio anterior
+com n=5. 
+2) Algoritmo
+Dois for: 
+1 que vai imprimindo os espaços do maior para o menor
+Outro que reusa o trabalho precedente com os asteriscos
+um console.log que concatena as duas var geradas?
+3) Colocando no codigo, graças a correçao de outro aluno:*/
 
-let triangleStars='';
 let n = 5;
-
-for (let i=0; i<n; i+=1) {
-    triangleStars += '*';
-    console.log(triangleStars);
+for (k=1;k<=n;k+=1){
+    let space = '';
+    for (j=n-k;j>0;j-=1){
+        space += " ";
     }
+let star='';
+for (i=n-k;i<n;i+=1) {
+    star += "*";
+}
+console.log(space+star);
+}
