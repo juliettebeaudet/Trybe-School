864a1ad9776a914832c61ccb369e6b9d5085a0c2
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-15 15:47:36 -0300

    ex turning former codes into functions

---
+++ exercises/4_4/ex-refatorar-codigo.js
@@ -0,0 +1,35 @@
+//Objetivo: retomar exercicios do 4.1 e transformar com functions.
+let a = 2;
+let b = 3;
+// //Faça programas para:
+// //Adição
+// let ad = a+b;
+// //Subtracão
+// let sub = a-b;
+// //Multiplicação
+// let mult = a*b
+// //Divisão
+// let div = a/b;
+// //Módulo
+// let mod = a%b;
+// console.log(ad, sub, mult, div, mod);
+
+function ad(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mult(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function mod(a, b) {
+    return a % b;
+}
