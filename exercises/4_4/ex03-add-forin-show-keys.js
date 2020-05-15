49afde44060b7310cbb4a48e4aacff21f7ebf67d
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-15 17:33:50 -0300

    Ex03 Solved, about showing only keys with for in

---
+++ exercises/4_4/ex03-add-forin-show-keys.js
@@ -0,0 +1,13 @@
+
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+info.recorrente = "Sim";
+
+for (let chave in info) {
+    console.log(chave);
+}
