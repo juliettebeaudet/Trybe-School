507d4e69d7a1ced1867f861621ecb380a312e89e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-15 17:25:03 -0300

    ex02 add property in object, solved

---
+++ exercises/4_4/ex02-add-property-object.js
@@ -0,0 +1,12 @@
+// Insira no objeto uma nova propriedade com o nome de chave “recorrente”
+// e o valor “Sim” e, em seguida, imprima o objeto no console.
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+info.recorrente = "Sim";
+console.log(info);
+
