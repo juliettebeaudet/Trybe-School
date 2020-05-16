5637fd80ec92db5d1e9ff58579663c3bb9923f12
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-15 21:05:03 -0300

    Ex IV solved, showing longest name in array

--- exercises/4_4/Ex-IV-Function-array.js
+++ exercises/4_4/Ex-IV-Function-array.js
@@ -2,16 +2,16 @@
 // Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
 // Valor esperado no retorno da função: Fernanda.
 
-let arrayForTest = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+let arrayForTest = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairooooooo', 'Joana'];
 function longestName(array) {
-    let indice = 0;
+    let index = 0;
     for (let i = 0; i < array.length; i += 1) {
         let array1 = "" + array[i];
         let array2 = "" + array[i+1];
         if (array1.length > array2.length) {
-            indice = i;
+            index = i;
         }
     }
-    return array[indice];
+    return array[index];
 }
 console.log(longestName(arrayForTest));
