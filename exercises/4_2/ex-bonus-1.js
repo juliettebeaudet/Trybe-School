ea676a27d55d5bd7306a13922de31d7ae7aacec6
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-16 12:40:20 -0300

    Ex bonus 1&2, added comments about bubble sorting process

--- exercises/4_2/ex-bonus-1.js
+++ exercises/4_2/ex-bonus-1.js
@@ -3,7 +3,7 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 1; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {//funciona com i=0 e i=1)
     for (let j = 0; j < i; j++) {
         if (numbers[i] < numbers[j]) {
             let position = numbers[i]; //precisa desse position como var auxiliar intermediaria
