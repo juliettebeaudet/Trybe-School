48ace0253e36316b97073170f5e80334f7f3ec4d
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-05 20:13:17 -0300

    still in the arrow transformation process

--- exercises/7_1/ex1.script.js
+++ exercises/7_1/ex1.script.js
@@ -3,26 +3,31 @@
 // é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
 
 function factorialize(n) {
-  if (n < 0)
-    return -1;
-  else if (n == 0)
-    return 1;
-  else {
-    return (n * factorialize(n - 1));
-  }
+    return (n * (n - 1));
 }
 console.log(factorialize(4));
+//aqui considerando que sempre n>1;
+//da resultado certo ou seja 24.
 
+const factorializeArrow = n => n*(n-1);
+console.log(factorializeArrow(4));
+//da resultado errado e diferente ou seja 12!!
 
 // Bônus (opcional): tente fazer o mesmo exercício de forma recursiva .
 // Spoiler: É possível resolver com uma linha.
 
 function factorialize(n) {
-  if (n === 0 || n === 1)
-    return 1;
-  for (var i = n - 1; i >= 1; i--) {
+  if (n === 0 || n === 1){
+    return 1;}
+  for (var i = n - 1; i >= 1; i-=1) {
     n *= i;
   }
   return n;
 }
 console.log(factorialize(5));
+
+const factorializeRecursive = n => {for (let i = n - 1; i >= 1; i--) {
+  n *= i;
+}
+}
+console.log(factorializeRecursive(4));
