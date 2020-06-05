8dead47dece01b13361d660c97f66e99bc1022e8
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-05 20:59:08 -0300

    solved one line factorize arrow function

--- exercises/7_1/ex1.script.js
+++ exercises/7_1/ex1.script.js
@@ -2,32 +2,68 @@
 // Na matemática, o fatorial de um número não negativo N, com a notação N!,
 // é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
 
+
+//primeiro a function do jeito que sei escrever
 function factorialize(n) {
-    return (n * (n - 1));
+  if (n < 0) {
+    return -1;
+  }
+  else if (n == 0) {
+    return 1;
+  }
+  else {
+    return (n * factorialize(n - 1));
+  }
 }
 console.log(factorialize(4));
 //aqui considerando que sempre n>1;
 //da resultado certo ou seja 24.
 
-const factorializeArrow = n => n*(n-1);
+//refarotando para function de tipo arrow
+const factorializeArrow = n => {
+
+  if (n < 0) {
+    return -1;
+  }
+  else if (n == 0) {
+    return 1;
+  }
+  else {
+    return (n * factorialize(n - 1));
+  }
+}
 console.log(factorializeArrow(4));
-//da resultado errado e diferente ou seja 12!!
+//retorna certo
 
 // Bônus (opcional): tente fazer o mesmo exercício de forma recursiva .
 // Spoiler: É possível resolver com uma linha.
 
+//primeiro com antiga sintax
 function factorialize(n) {
-  if (n === 0 || n === 1){
-    return 1;}
-  for (var i = n - 1; i >= 1; i-=1) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  for (var i = n - 1; i >= 1; i -= 1) {
     n *= i;
   }
   return n;
 }
 console.log(factorialize(5));
+//retorna certo
 
-const factorializeRecursive = n => {for (let i = n - 1; i >= 1; i--) {
-  n *= i;
+//escrevendo com function de tipo arrow
+const factorializeRecursive = n => {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  for (var i = n - 1; i >= 1; i -= 1) {
+    n *= i;
+  }
+  return n;
 }
-}
-console.log(factorializeRecursive(4));
+console.log(factorializeRecursive(5));
+//retorna certo
+
+//em uma linha apenas, desconsiderando possibilidade de n<1
+const factorializeRecursiveLine = n => { for(var i = n - 1; i >= 1; i -= 1){n *= i;} return n;}
+console.log(factorializeRecursiveLine(5));
