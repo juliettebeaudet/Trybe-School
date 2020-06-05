b08e0ace2b978caa177ff5fc52b140eb40e6a47f
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-05 19:42:28 -0300

    resolvendo function factorize two ways, still have to do it arrow style

--- exercises/7_1/ex1.script.js
+++ exercises/7_1/ex1.script.js
@@ -1,15 +1,28 @@
 // 1. Crie uma função que receba um número e retorne seu fatorial.
 // Na matemática, o fatorial de um número não negativo N, com a notação N!,
 // é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+
+function factorialize(n) {
+  if (n < 0)
+    return -1;
+  else if (n == 0)
+    return 1;
+  else {
+    return (n * factorialize(n - 1));
+  }
+}
+console.log(factorialize(4));
+
+
 // Bônus (opcional): tente fazer o mesmo exercício de forma recursiva .
 // Spoiler: É possível resolver com uma linha.
 
-let factorized = 0;
-function factorize(n) {
-for (let i = 0; i <= n; i += 1) {
-    }
-    factorized = n*i;
-    return factorized;
+function factorialize(n) {
+  if (n === 0 || n === 1)
+    return 1;
+  for (var i = n - 1; i >= 1; i--) {
+    n *= i;
   }
-
-  console.log(factorize(5));
+  return n;
+}
+console.log(factorialize(5));
