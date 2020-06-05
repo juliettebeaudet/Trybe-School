be5d71b8d1df047abff4aac34d0669cb952fa7c6
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-05 19:43:04 -0300

    began function longestWord

--- exercises/7_1/ex2.script.js
+++ exercises/7_1/ex2.script.js
@@ -1,3 +1,7 @@
 // 2. Crie uma função que receba uma frase e retorne qual a maior palavra.
 // Exemplo:
 // longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
+// function longestWord(sentence) {
+
+// }
