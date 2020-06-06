c5334460161125598d9361b993015e273013ac19
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-05 21:28:46 -0300

    began ex4

--- exercises/7_1/ex4.script.js
+++ exercises/7_1/ex4.script.js
@@ -11,6 +11,9 @@
 // Retorno: “Tryber Bebeto aqui!”
 // Um array com escopo global, que é o escopo do arquivo JS, nesse caso, contendo cinco strings com suas principais skills.
 
+// const function1 = (string) => ''
+// console.log(function1('Bebeto'));
+
 // Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro.
 // Essa função deve concatenar as skills do array global à string que foi passada para
 // a Função 2 via parâmetro.
