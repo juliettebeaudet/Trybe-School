2f30facefc97c3c5c424671d7ecf19c209130cd9
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-05 21:49:30 -0300

    solved ex2, arrow function returns longest word in sentence param

--- exercises/7_1/ex2.script.js
+++ exercises/7_1/ex2.script.js
@@ -2,15 +2,30 @@
 // Exemplo:
 // longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
 
-function longestWord(sentence) {
+
+// function longestWord(sentence) {
+//   let wordsArray = sentence.split(" ");
+//   let longWord = "";
+//   for (i=0; i<wordsArray.length; i+=1){
+//     if (wordsArray[i].length>longWord.length){
+//       longWord = wordsArray[i];
+//     }
+//   }
+//   return longWord;
+// }
+
+// console.log(longestWord("This is a test, aohflfhaoh"));
+
+//now, same with arrow function 
+const longestWord = sentence => {
   let wordsArray = sentence.split(" ");
   let longWord = "";
   for (i=0; i<wordsArray.length; i+=1){
+    if (wordsArray[i].length>longWord.length){
+      longWord = wordsArray[i];
+    }
   }
+  return longWord;
 }
 
-console.log(longestWord("bla blou"));
-
-
-// var str = "How are you doing today?";
-// var res = str.split(" ");
+console.log(longestWord("This is a test, blaaaaaaaaaah"));
