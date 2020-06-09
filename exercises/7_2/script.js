743a4b2ece56e7e320c30a2311ba20553a40eefe
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-09 11:04:21 -0300

    ex7 solved using Object.values

--- exercises/7_2/script.js
+++ exercises/7_2/script.js
@@ -76,4 +76,24 @@ function numberStudents(obj, key){
 }
 console.log(numberStudents(allLessons, 'numeroEstudantes'));
 
+// 7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+// Por exemplo:
+// console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
+
+function getValueByNumber(lesson, i){
+const allValues = Object.values(lesson);
+const valuePosition = allValues[i];
+return valuePosition;
+}
+
+console.log(getValueByNumber(lesson1, 2));
+
+// 8. Crie uma função que verifique se o par (chave / valor) existe na função.
+// Essa função deve possuir três parâmetros, sendo eles:
+// o objeto, o nome da chave e o valor da chave. Exemplo:
+// console.log(verifyPair(lesson3, 'turno', 'noite'));
+// // Output: true,
+// console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+// // Output: false
 
