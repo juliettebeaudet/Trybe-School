3b87f55d8ea59edb9fd61bc3101a04ed64822962
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-09 19:05:44 -0300

    ex4 solved with assert.deepEqual

---
+++ exercises/7_3/ex4-script.js
@@ -0,0 +1,26 @@
+// 4. A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array
+// sem o elemento item caso ele exista no array
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+const expected = myRemove([1, 2, 3, 4], 3);
+assert.deepEqual(expected, [1, 2, 4], 'n sumiu o 3');
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.deepEqual(expected, [1, 2, 3, 4], 'dar erro aqui é bom');
+// Verifique se o array passado por parâmetro não sofreu alterações
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+const expectedTwo = myRemove([1, 2, 3, 4], 5);
+assert.deepEqual(expectedTwo, [1, 2, 3, 4], 'era para nada acontecer pois 5 n é item do array');
