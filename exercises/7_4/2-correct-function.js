1820e7a30f0792ec5fc556f7a94513b63dce38b5
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-10 19:12:41 -0300

    began ex2

--- exercises/7_4/2-correct-function.js
+++ exercises/7_4/2-correct-function.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
 // escreva a função wordLengths para passar nos testes abaixo:
-
+function wordLengths(palavras){
+let arrayNumbers = [];
+for (i=0; i<palavras.length; i+=0){
+  arrayNumbers.push(palavras[i]);
+}
+  return arrayNumbers;
+}
 //
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
