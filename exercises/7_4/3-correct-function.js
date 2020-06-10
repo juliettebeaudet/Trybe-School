344974284ff1e71079c9f36efc3cea2f54c40bda
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-10 19:32:12 -0300

    ex3 solved and tested function addAllnumbers

--- exercises/7_4/3-correct-function.js
+++ exercises/7_4/3-correct-function.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
 // escreva a função addAllnumbers para passar nos testes abaixo:
-
+const addAllnumbers = (num) => {
+  let finalNum = 0;
+  for (i=0; i<num.length; i+=1){
+    finalNum += num[i];
+  }
+  return finalNum;
+}
 //
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
