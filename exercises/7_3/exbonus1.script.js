e60e38f2d07e618664b8a3d6e63032a7a6fc65fc
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-12 16:05:56 -0300

    exbonus1 solved with while loop

--- exercises/7_3/exbonus1.script.js
+++ exercises/7_3/exbonus1.script.js
@@ -6,35 +6,41 @@ function getChange(payable, paid) {
 
   // escreva seu código aqui...
   if (remaining < 0) {
-    throw new Error('paid value is not enough'); //jeito de chamar error mais preciso que alert
+    throw new Error('paid value is not enough'); //jeito de chamar error em vez de alert
   }
-  else if (remaining >= 0) {
+  // else if (remaining == 0) {
+  //   alert('thank you for inserting the exact amount'); //era opcional e alert deu erro undefined por algum motivo
+  // }
+
+  while (remaining > 0) { //else if n funcionava
     for (i = 0; i < coins.length; i += 1) {
-      if (coins[i] <= remaining) { //enquanto o coin for menor ou igual que o resto a ser dado
-        remaining -= coins[i]; //...coin pode ser adicionado para diminuir o resto a ser reembolsado
-        change.push(coins[i]); 
+      if (coins[i] <= remaining) { //se o coin é de um valor menor do que o reembolsido devido
+        remaining -= coins[i]; //...ele pode ser adicionado para quitar o valor do reembolso aos poucos
+        change.push(coins[i]); //e por consequência ser adicionado ao array dos coins usados para reembolso
+        break;
       }
     }
-    return change; //retornando array dos coins usados para reembolso
   }
+  return change;
 }
-  // os testes unitários já estão prontos, e sua implementação deve passar por todos eles.
-  const assert = require('assert');
 
-  let result = getChange(1, 1); // no change/coins just an empty array
-  let expected = [];
-  assert.deepEqual(result, expected);
+// os testes unitários já estão prontos, e sua implementação deve passar por todos eles.
+const assert = require('assert');
 
-  result = getChange(215, 300); // expect an array containing [50, 20, 10, 5]
-  expected = [50, 20, 10, 5];
-  assert.deepEqual(result, expected);
+let result = getChange(1, 1); // no change/coins just an empty array
+let expected = [];
+assert.deepEqual(result, expected);
 
-  result = getChange(486, 600); // expect an array containing [100, 10, 2, 2]
-  expected = [100, 10, 2, 2];
-  assert.deepEqual(result, expected);
+result = getChange(215, 300); // expect an array containing [50, 20, 10, 5]
+expected = [50, 20, 10, 5];
+assert.deepEqual(result, expected);
 
-  result = getChange(12, 400); // expect an array containing [200, 100, 50, 20, 10, 5, 2, 1]
-  expected = [200, 100, 50, 20, 10, 5, 2, 1];
-  assert.deepEqual(result, expected);
+result = getChange(486, 600); // expect an array containing [100, 10, 2, 2]
+expected = [100, 10, 2, 2];
+assert.deepEqual(result, expected);
 
-  assert.throws(() => { getChange(100, 10); }, /^Error: paid value is not enough$/);
+result = getChange(12, 400); // expect an array containing [200, 100, 50, 20, 10, 5, 2, 1]
+expected = [200, 100, 50, 20, 10, 5, 2, 1];
+assert.deepEqual(result, expected);
+
+assert.throws(() => { getChange(100, 10); }, /^Error: paid value is not enough$/);
