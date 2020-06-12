ae507b7ea9947e48aeae90203beb50caf52db6e1
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-12 11:36:35 -0300

    ex4 completed and changed deepEqual to notdeepEqual

--- exercises/7_3/ex4-script.js
+++ exercises/7_3/ex4-script.js
@@ -15,12 +15,14 @@ function myRemove(arr, item) {
 
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-const expected = myRemove([1, 2, 3, 4], 3);
-assert.deepEqual(expected, [1, 2, 4], 'n sumiu o 3');
-// // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-// assert.deepEqual(expected, [1, 2, 3, 4], 'dar erro aqui é bom');
+const actual = myRemove([1, 2, 3, 4], 3);
+assert.deepEqual(actual, [1, 2, 4], 'n sumiu o 3');
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notdeepEqual(actual, [1, 2, 3, 4], 'retornou array errado');
 // Verifique se o array passado por parâmetro não sofreu alterações
-
+const arr = [1, 2, 3, 4];
+myRemove(arr, 3);
+assert.deepEqual(arr, [1, 2, 3, 4], 'alterou array parâmetro?');
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 const expectedTwo = myRemove([1, 2, 3, 4], 5);
 assert.deepEqual(expectedTwo, [1, 2, 3, 4], 'era para nada acontecer pois 5 n é item do array');
