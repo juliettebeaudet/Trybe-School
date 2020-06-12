1c84e0945dcf1fd9ad8efe494bf8075a882a6f98
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-12 10:49:09 -0300

    ex4 solved with methods replace and sort

--- exercises/7_1/ex4.script.js
+++ exercises/7_1/ex4.script.js
@@ -11,18 +11,12 @@
 // Retorno: “Tryber Bebeto aqui!”
 // Um array com escopo global, que é o escopo do arquivo JS, nesse caso, contendo cinco strings com suas principais skills.
 
-
 const function1 = (string) => {
-let mySentence = 'Tryber x aqui!';
-let wordArray = mySentence.split(' ');
-for (i=0; i<wordArray.length; i+=1){
-  if (wordArray[i] == 'x'){
-  `${string}` = wordArray[i];
-  }
-  return mySentence;
-}
-}
-console.log(function1('BlaBleBliBloBlu')); //returns error
+  const definedString = 'Tryber x aqui!';
+  const newString = definedString.replace('x', string); //para substituir
+  return newString;
+};
+console.log(function1('BlaBleBliBloBlu'));
 
 // Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro.
 // Essa função deve concatenar as skills do array global à string que foi passada para
@@ -35,3 +29,18 @@ console.log(function1('BlaBleBliBloBlu')); //returns error
 // JavaScript;
 // HTML; …
 // #goTrybe”.
+
+let arraySkills = ['HTML', 'CSS', 'Javascript', 'Git', 'Softskill']; //sem const porque vai ser reordenado
+const function2 = (string) => {
+  arraySkills = arraySkills.sort(); //para ordem alfabética
+  const stringTwo = `${function1(string)}`; //para pegar retorno da function1
+  return `${stringTwo} 
+  Minhas cinco principais habilidades são:
+    ${arraySkills[0]}
+    ${arraySkills[1]}
+    ${arraySkills[2]}
+    ${arraySkills[3]}
+    ${arraySkills[4]} 
+  #goTrybe`; //para retornar concatenado, escrito com template literals
+};
+console.log(function2('')); //ok mas repete a string com os paramêtros das 2 functions 
