7103dcf14c1cd9c81a045b8d62ace3b70648b72d
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-16 16:25:29 -0300

    ex2 solved with forEach

---
+++ exercises/8_1/exercise2-forEach.js
@@ -0,0 +1,80 @@
+// 2. Retorne o nome do livro de menor nome.
+
+const assert = require("assert");
+
+const books = [
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+function smallerName() {
+  let nameBook = 'blaaaaaaaaaaaaaaaaaa';
+  // escreva aqui o seu código
+  books.forEach((book) => {
+    if (book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
+}
+
+assert.equal(smallerName(), "Duna");
