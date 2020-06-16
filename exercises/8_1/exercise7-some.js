27552a2e7e4ab6a4723bcc2c8ce49da6844cfa77
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-16 18:28:48 -0300

    ex7 solved using some

---
+++ exercises/8_1/exercise7-some.js
@@ -0,0 +1,77 @@
+// 7. Faça uma função que retorne true, se algum livro foi lançado na década de 80,
+// e false, caso contrário.
+
+const assert = require("assert");
+
+const books = [
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = true;
+
+function someBookWasReleasedOnThe80s() {
+  // escreva seu código aqui
+  const eighties = books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
+  return eighties;
+}
+
+assert.equal(someBookWasReleasedOnThe80s(), expected_result);
