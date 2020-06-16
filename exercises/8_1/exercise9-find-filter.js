c1c8311297e91b3b53f4e1438c89d50f5db66495
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-16 19:55:03 -0300

    ex9 solved the easy way with find and direct author name

---
+++ exercises/8_1/exercise9-find-filter.js
@@ -0,0 +1,88 @@
+// 9. Encontre o nome do livro escrito pela pessoa cujo nome registrado
+// começa com três iniciais (terminam com um ponto).
+// babysteps:
+// 1) interpretar: devo retornar apenas esse nome de livro, ou seja 'O Senhor dos Anéis' (J.R.R.)
+// 2) botar um const que vai ser atribuido um find/filter para achar o book onde tà o nome de author com três iniciais
+// aqui três caminhos: easy é escrever diretamente J.R.R. , hard é includes e contador até 3 pontos, ou regex.
+// 3) retornar aquele book .name
+
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  // escreva seu código aqui
+const initials = books.find(book => book.author.name == 'J. R. R. Tolkien');
+return initials.name;
+}
+
+assert.deepEqual(authorWith3DotsOnName(), expected_result);
+
+// para fazer o caminho dos três pontos, seguem dicas:
+// "eu usei o filter só para diminuir o número de iterações do forEach,
+// filtrando os elementos para só aqueles que possuem no mínimo um ponto final no nome"
+// "com um contador, J.K. só tem dois pontos e J.R.R. tem três e a função
+// só retorna caso o contador seja igual a três"
