b8fa9c96d6430ae6fd8dc5f7326946793f6a864e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-16 18:45:21 -0300

    ex8 solved with filter and sort on two consecutive arrays

---
+++ exercises/8_1/exercise8-filter-sort.js
@@ -0,0 +1,102 @@
+// 8. Crie um array ordenado(composto) pelos livros com mais de 60 anos de publicação
+// e ordene-o pelo livro mais velho.
+
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  }
+]
+
+function oldBooks() {
+  // escreva seu código aqui
+  const moreThanSixty = books.filter(book => book.releaseYear <= 2020 - 60); // primeiro criar um array com apenas livros de mais de 60 anos
+  const olderBooks = moreThanSixty.sort(function(a,b){ //segundo ordenar esse novo array
+    return a.releaseYear - b.releaseYear; //isso é a formula 'pronta' para ordenar numbers em ordem crescente com sort
+  });
+  return olderBooks;
+}
+
+assert.deepEqual(oldBooks(), expected_result);
