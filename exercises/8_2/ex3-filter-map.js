4f2ee1343ebbe805ec649943a3d9da374ea5d8ad
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-17 19:19:06 -0300

    ex3 solved using filter and map

---
+++ exercises/8_2/ex3-filter-map.js
@@ -0,0 +1,81 @@
+// 3. Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu'
+]
+
+function oldBooks() {
+  // escreva seu código aqui
+  const moreThanSixty = books.filter(book => book.releaseYear <= 2020 - 60); // primeiro criar um array com apenas livros de mais de 60 anos (ou seja retorna ainda os objetos inteiros)
+  const oldBooksArray = moreThanSixty.map(book => book.name); //para criar um array especificamente dos nomes dos livros
+  return oldBooksArray;
+}
+
+assert.deepEqual(oldBooks(), expected_result);
