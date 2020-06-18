29116f5e075f3838464a13d75fe2f174585774a3
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-18 18:50:02 -0300

    ex2 on progress, rest

--- exercises/8_3/ex2-rest.js
+++ exercises/8_3/ex2-rest.js
@@ -5,10 +5,10 @@
 const assert = require('assert')
 
 // escreva sum abaixo
+const sum = (...itens) => sum.reduce((item) => item1 + item2);//////NOPE
 
 assert.equal(sum(), 0)
 assert.equal(sum(1), 1)
 assert.equal(sum(1, 2), 3)
 assert.equal(sum(1, 2, 3), 6)
 assert.equal(sum(1, 2, 3, 4), 10)
-    
