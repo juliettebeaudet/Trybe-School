2012e7d446dfb2106d22d059fd2a94eea9eed4ab
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-18 19:00:10 -0300

    ex3 began

--- exercises/8_3/ex3-obj-destr.js
+++ exercises/8_3/ex3-obj-destr.js
@@ -19,7 +19,11 @@ const gunnar = {
 }
 
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(", ")}.`
+const personLikes = () => {
+  const {name, age, likes, nationality} = alex;
+  const {name, age, likes, nationality} = gunnar; 
+  return `${name} is ${age} years old and likes ${likes.join(", ")}.`
+};
 assert.equal(personLikes(alex), "Alex is 26 years old and likes fly fishing.")
 assert.equal(personLikes(gunnar), "Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.")
     
