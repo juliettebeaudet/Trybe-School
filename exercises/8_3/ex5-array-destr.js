75f641c45f45ed4f918a79f2305ee22e3ec004a3
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-18 19:51:24 -0300

    ex5 solved using array destructuring within function parameter

--- exercises/8_3/ex5-array-destr.js
+++ exercises/8_3/ex5-array-destr.js
@@ -7,10 +7,9 @@ const assert = require('assert')
 const myList = [1, 2, 3]
 
 // escreva swap abaixo
-
+const swap = ([a, b, c]) => [c, b, a]; //isso nos params da function swap é um array destructuring
 const swappedList = swap(myList)
 
 assert.equal(swappedList[0], 3)
 assert.equal(swappedList[1], 2)
 assert.equal(swappedList[2], 1)
-    
