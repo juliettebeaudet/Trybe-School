eee9d89c8c80e1db4b37057f525ca3d19a5d41ce
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-18 17:11:13 -0300

    ex8 solved using default params

--- exercises/8_3/ex8-default-params.js
+++ exercises/8_3/ex8-default-params.js
@@ -4,8 +4,8 @@
 const assert = require('assert')
 
 // escreva greet abaixo
+const greet = (name = '', greeting = 'Hi') => `${greeting} ${name}`;
 
 assert.equal(greet("John"), "Hi John")
 assert.equal(greet("John", "Good morning"), "Good morning John")
 assert.equal(greet("Isabela", "Oi"), "Oi Isabela")
-    
