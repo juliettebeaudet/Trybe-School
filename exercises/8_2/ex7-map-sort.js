437a58662f75566d32563fba443fd2bd8bec2c02
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-18 08:38:54 -0300

    ex7 half way, map done

--- exercises/8_2/ex7-map-sort.js
+++ exercises/8_2/ex7-map-sort.js
@@ -4,65 +4,65 @@
 // O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha
 // considerando suas idades quando o livro foi lançado.
 
-const assert = require('assert');
+const assert = require("assert");
 
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948
+      name: "George R. R. Martin",
+      birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
-      birthYear: 1947
+      name: "Stephen King",
+      birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -72,32 +72,42 @@ const books = [
 const expected_result = [
   {
     age: 31,
-    author: 'Isaac Asimov'
+    author: "Isaac Asimov",
   },
   {
     age: 38,
-    author: 'H. P. Lovecraft'
+    author: "H. P. Lovecraft",
   },
   {
     age: 39,
-    author: 'Stephen King'
+    author: "Stephen King",
   },
   {
     age: 43,
-    author: 'George R. R. Martin'
+    author: "George R. R. Martin",
   },
   {
     age: 45,
-    author: 'Frank Herbert'
+    author: "Frank Herbert",
   },
   {
     age: 62,
-    author: 'J. R. R. Tolkien'
-  }
+    author: "J. R. R. Tolkien",
+  },
 ];
 
 function nameAndAge() {
   // escreva seu código aqui
+  // 1) criar o novo array de objetos com map e template literals
+  const objectsAuthors = books.map((book) => {
+    const releaseAge = book.releaseYear - book.author.birthYear;
+    const authorName = book.author.name;
+    return `{
+    age: ${releaseAge},
+  author: ${authorName},
+  }`;
+  });
+  // 3) ordenar esses objetos com sort
 }
 
 assert.deepEqual(nameAndAge(), expected_result);
