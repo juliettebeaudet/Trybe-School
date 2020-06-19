724b154302cc11b4680e63c6431d5acf674bba66
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-18 21:16:16 -0300

    ex7 began

--- exercises/8_3/ex7-obj-destr.js
+++ exercises/8_3/ex7-obj-destr.js
@@ -21,6 +21,7 @@ const ships = [
 ]
 
 // escreva shipLength abaixo
+const shipLength = (ship) => 
 
 assert.equal(shipLength(ships[0]), "Titanic is 269.1 meters long")
 assert.equal(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
