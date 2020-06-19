d862d15296da175e0036ea1c4a86bab5843814fe
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-18 21:20:45 -0300

    ex5 solved with array destructuring in one line

--- exercises/8_3/ex5-array-destr.js
+++ exercises/8_3/ex5-array-destr.js
@@ -2,14 +2,19 @@
 // retorna um novo array com o primeiro e terceiro elementos trocados.
 // Detalhe: você precisa fazer essa função gastando 1 linha só:
 
-const assert = require('assert')
+const assert = require("assert");
 
-const myList = [1, 2, 3]
+const myList = [1, 2, 3];
 
 // escreva swap abaixo
+// const swap = (array) => {
+//   const [a, b, c] = array;
+//   return [c, b, a];
+// };
+// reduzindo em uma linha:
 const swap = ([a, b, c]) => [c, b, a]; //isso nos params da function swap é um array destructuring
-const swappedList = swap(myList)
+const swappedList = swap(myList);
 
-assert.equal(swappedList[0], 3)
-assert.equal(swappedList[1], 2)
-assert.equal(swappedList[2], 1)
+assert.equal(swappedList[0], 3);
+assert.equal(swappedList[1], 2);
+assert.equal(swappedList[2], 1);
