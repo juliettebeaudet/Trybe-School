3afe6d2c7674f10dfa4aa8f96e63adde2672631d
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-24 12:10:13 -0300

    ex1 solved, turned 7.3 assert into 9.1 jest tests

--- exercises/9_1/exjest.js
+++ exercises/9_1/exjest.js
@@ -7,4 +7,54 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum
+function myIndexOf(arr, item) {
+  let searchedIndex = -1;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item === arr[i]) {
+      searchedIndex = i;
+    }
+  }
+  return searchedIndex;
+}
+
+function mySum(arr) {
+  let result = 0;
+  for (let item in arr) {
+    result += arr[item];
+  }
+  return result;
+}
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+function defined(string) {
+  const thereIs = string;
+}
+
+function thereIs() {};
+
+module.exports = {
+  sum,
+  myIndexOf,
+  mySum,
+  myRemove,
+  myFizzBuzz,
+  defined,
+  thereIs
+}
--- exercises/9_1/exjest.test.js
+++ exercises/9_1/exjest.test.js
@@ -1,14 +1,42 @@
 const exjest = require('./exjest')
 
-test('deve ser 9', () => {
-  expect(exjest(4, 5)).toBe(9);
+test('ex 1 sum 9', () => {
+  expect(exjest.sum(4, 5)).toBe(9);
 });
 
-test('deve ser zero', () => {
-  expect(exjest(0, 50)).toBe(50);
+test('ex 1 sum 50', () => {
+  expect(exjest.sum(0, 50)).toBe(50);
 });
 
-// test('testar string', () => {
+// test('ex 1 testar string error', () => {
 //   expect(exjest(4,'5')).toBe(9);
 // });
-// isso falhou como previsto, param tem que ser number.
+// isso falhou como previsto: mensagem param tem que ser number.
+
+test('ex2 testar array', () => {
+  expect(exjest.myIndexOf([1, 2, 3, 4], 3)).toBe(2);
+});
+
+test('ex2 testar array com elemento exterior', () => {
+  expect(exjest.myIndexOf([1, 2, 3, 4], 5)).toBe(-1);
+});
+
+test('ex3 testar que soma dà 0', () => {
+  expect(exjest.mySum([1, -2, -3, 4])).toBe(0);
+});
+
+test('ex4 testar inegalidade dos elementos de arrays', () => {
+  expect(exjest.myRemove([1, 2, 3, 4], 3)).not.toBe([1, 2, 3, 4]);
+});
+
+test('ex6 testar buzz', () => {
+  expect(exjest.myFizzBuzz(10)).toBe('buzz');
+});
+
+test('ex7 const é definida', () => {
+  expect(exjest.defined('bla')).not.toBe('undefined');
+});
+
+test('ex8 function é definida', () => {
+  expect(typeof exjest.thereIs).toBe('function');
+});
