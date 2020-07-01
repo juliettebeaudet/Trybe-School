b1e28878271e16a86d08c08accb5d8156e710400
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-01 19:43:39 -0300

    began ex2 promise test

--- exercises/10_1/ex2.test.js
+++ exercises/10_1/ex2.test.js
@@ -19,3 +19,16 @@ const findUserById = (id) => {
 const getUserName = (userID) => {
   return findUserById(userID).then(user => user.name);
 }
+
+// 2. Utilizando a sintaxe de Promise, faça um teste que verifique o resultado da função getUserName
+// para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário
+// não é encontrado.
+
+// Dica: Veja os dados falsos utilizados no banco de dados, disponíveis na variável users,
+// para saber quais IDs existem.
+
+// describe ('bla', () => {
+//   test('blabla', () => {
+//     return .then()
+//   })
+// })
--- exercises/10_1/ex3.test.js
+++ exercises/10_1/ex3.test.js
@@ -1,6 +1,9 @@
 // O código a seguir simula uma chamada ao banco de dados para buscar usuários.
 // O resultado dessa busca é uma *Promise*, que é utilizada pelo método `getUserName`.
 
+// 3. Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await.
+// Dica: Utilize o try/catch para o caso de erro.
+
 const users = {
   4: { name: 'Mark' },
   5: { name: 'Paul' }
