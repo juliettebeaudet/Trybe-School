47b328d048fb5a92a88f1486191430b2a7613f83
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-01 16:28:53 -0300

    testes da aula

---
+++ exercises/10_1/course-test-promise.js
@@ -0,0 +1,35 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalsByType = (type) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = Animals.filter((animal) => animal.type === type);
+      if (arrayAnimals.length !== 0) {
+        return resolve(arrayAnimals);
+      }
+
+      return reject({ error: 'Não possui esse tipo de animal.' });
+    }, 100);
+  })
+);
+
+const getListAnimals = (type) => (
+  findAnimalsByType(type).then(list => list)
+);
+
+describe('Quando o tipo do animal existe', () => {
+  test('Retorne a lista de animais', () => {
+    expect.assertions(2);
+    return getListAnimals('Dog').then(listDogs => {
+      expect(listDogs[0].name).toEqual('Dorminhoco');
+      expect(listDogs[1].name).toEqual('Soneca');
+    });
+  });
+});
+
+// Rode o teste e verifique se ele não está dando algum falso-positivo.
+// Mude o Dorminhoco para Agitado. Viu? O teste não passou, logo não possui falso-positivos.
