18c031b0de16164ec8bd629f7fb55402f9da1b4e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-06 19:00:40 -0300

    ex2 solved with mockImplementationOnce

--- exercises/10_2/ex-random-script.test.js
+++ exercises/10_2/ex-random-script.test.js
@@ -1,16 +1,35 @@
+const random = require('./ex-random-script.js');
+
 // 1. Utilizando o mock, defina o retorno padrão como 10.
 // Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
 
-const random = require('./ex-random-script.js');
+// test('ver se function foi chamada, o retorno e quantas vezes chamada', () => {
+//   random.randomNumber = jest
+//   .fn()
+//   .mockReturnValue(23);
 
-test('ver se function foi chamada, o retorno e quantas vezes chamada', () => {
+//   random.randomNumber();
+
+//   expect(random.randomNumber()).toBe(23);
+//   expect(random.randomNumber).toHaveBeenCalled();
+//   expect(random.randomNumber).toHaveBeenCalledTimes(2);
+// });
+
+// 2. Crie uma nova implementação, que deve receber dois parâmetros
+// e retornar a divisão do primeiro pelo segundo.
+// Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
+
+test('testar mudando o comportamento da function', () => {
   random.randomNumber = jest
   .fn()
-  .mockReturnValue(23);
+  .mockImplementationOnce((a, b) => a / b);
 
-  random.randomNumber();
+  // random.randomNumber(12, 4);
 
-  expect(random.randomNumber()).toBe(23);
-  expect(random.randomNumber).toHaveBeenCalled();
-  expect(random.randomNumber).toHaveBeenCalledTimes(2);
+  // expect(random.randomNumber).toHaveBeenCalled();
+  // expect(random.randomNumber).toHaveBeenCalledTimes(1);
+  // expect(random.randomNumber).toHaveBeenCalledWith(12, 4);
+  expect(random.randomNumber(12, 4)).toBe(3); // único teste em caso de Once, sem once colocar os outros
 });
+
+
