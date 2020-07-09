c7abe9b603d31f9f3ef83f72f9713d34bd111cad
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-09 13:40:01 -0300

    created array

--- exercises/11_1/react11.1/src/App.js
+++ exercises/11_1/react11.1/src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// insira a função a seguir acima do seu App:
+// 1. insira a função a seguir acima do seu App:
 const task = (value) => {
   return (
     <li>{value}</li>
   );
 }
 
-// agora, chame a função dentro do seu componente App (não se esqueça da sintaxe JSX!) - ou seja {}.
+// 3.crie uma array de compromissos e use a função map para que cada item do array apareça,
+// como um item de lista, no seu componente App.
 
+const tasks = (intro) => {
+  const arrayTasks = ['item 1', 'item 2', 'item 3'];
+  return arrayTasks.map(i => intro + i);
+}
 
 function App() {
   return (
@@ -20,8 +25,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <ul>{task('item 1')}</ul>
-        const arrayTasks = ['item 1', 'item 2', 'item 2'];
+        {/* 2. chame dentro dentro do seu componente App (não se esqueça da sintaxe JSX!) - ou seja {}. */}
+        {/* <ul>{task('item 1')}</ul> */}
+        <ul>{tasks('Devo fazer')}</ul>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
