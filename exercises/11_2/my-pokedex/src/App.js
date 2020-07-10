240233a28126ac0cb69edb57e4f3a4fa322f5442
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-10 13:25:48 -0300

    componentes criados

--- exercises/11_2/my-pokedex/src/App.js
+++ exercises/11_2/my-pokedex/src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Allpokemons from './data';
+
+// 1. Pokemon: esse componente recebe como entrada um objeto que contém informações referentes
+// a um pokemon específico. Esse componente precisa retornar as seguintes informações
+// para serem mostradas para quem usar a aplicação: nome, tipo, peso médio (com unidade), imagem.
+
+class Pokemon extends React.Component{
+  render() {
+    return 
+  }
+}
+
+// 2. Pokedex: esse componente representa a enciclopédia de pokemons.
+// Esse componente recebe como entrada uma lista de pokemons para serem mostrados na tela.
+// Para cada um desses pokemons recebidos, Pokedex chama o componente Pokemon.
+
+class Pokedex extends React.Component{
+  render() {
+    return 
+  }
+}
 
 function App() {
   return (
