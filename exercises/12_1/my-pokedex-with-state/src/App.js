e1f7b9c134d693215ecb1ecacdf17d1579677387
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-13 18:27:07 -0300

    initiating pokemon with state

---
+++ exercises/12_1/my-pokedex-with-state/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Allpokemons from './data';
+
+// 1. Pokemon: esse componente recebe como entrada um objeto que contém informações referentes
+// a um pokemon específico. Esse componente precisa retornar as seguintes informações
+// para serem mostradas para quem usar a aplicação: nome, tipo, peso médio (com unidade), imagem.
+
+class Pokemon extends React.Component{
+  render() {
+    return (
+      <section className="pokemons">
+      <div className="each_pokemon">
+      <p>{this.props.name}</p>
+      <p>{this.props.type}</p>
+      <p>{`${this.props.weight} ${this.props.unit}`}</p>
+      <img src={this.props.image} />
+      </div>
+      </section>
+    );   
+  }
+}
+
+// 2. Pokedex: esse componente representa a enciclopédia de pokemons.
+// Esse componente recebe como entrada uma lista de pokemons para serem mostrados na tela.
+// Para cada um desses pokemons recebidos, Pokedex chama o componente Pokemon.
+
+class Pokedex extends React.Component{
+  render() {
+    return Allpokemons.map(pokemon => (
+    <Pokemon
+      name={pokemon.name}
+      type={pokemon.type}
+      image={pokemon.image}
+      weight={pokemon.averageWeight.value}
+      unit={pokemon.averageWeight.measurementUnit}
+    />
+    ));
+  }
+}
+  
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
---
+++ exercises/12_1/my-pokedex-with-state/src/data.js
@@ -0,0 +1,103 @@
+const pokemons = [
+  {
+      id: 25,
+      name: "Pikachu",
+      type: 'Electric',
+      averageWeight: {
+          value: "6.0",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)"
+  },
+  {
+      id: 4,
+      name: "Charmander",
+      type: 'Fire',
+      averageWeight: {
+          value: "8.5",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)"
+  },
+  {
+      id: 10,
+      name: "Caterpie",
+      type: 'Bug',
+      averageWeight: {
+          value: "2.9",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)"
+  },
+  {
+      id: 23,
+      name: "Ekans",
+      type: 'Poison',
+      averageWeight: {
+          value: "6.9",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/1/18/Spr_5b_023.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)"
+  },
+  {
+      id: 65,
+      name: "Alakazam",
+      type: 'Psychic',
+      averageWeight: {
+          value: "48.0",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)"
+  },
+  {
+      id: 151,
+      name: "Mew",
+      type: 'Psychic',
+      averageWeight: {
+          value: "4.0",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)"
+  },
+  {
+      id: 78,
+      name: "Rapidash",
+      type: 'Fire',
+      averageWeight: {
+          value: "95.0",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/5/58/Spr_5b_078.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)"
+  },
+  {
+      id: 143,
+      name: "Snorlax",
+      type: 'Normal',
+      averageWeight: {
+          value: "460.0",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)"
+  },
+  {
+      id: 148,
+      name: "Dragonair",
+      type: 'Dragon',
+      averageWeight: {
+          value: "16.5",
+          measurementUnit: "kg"
+      },
+      image: "https:\/\/cdn.bulbagarden.net/upload/2/2c/Spr_5b_148.png",
+      moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)"
+  }
+];
+
+export default pokemons;
