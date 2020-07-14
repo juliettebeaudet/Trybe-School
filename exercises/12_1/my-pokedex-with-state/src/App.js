62b0aefa10c955ec30e0734e36fa9cfe4b02cf93
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-14 12:42:05 -0300

    avançando no entendimento state react

--- exercises/12_1/my-pokedex-with-state/src/App.js
+++ exercises/12_1/my-pokedex-with-state/src/App.js
@@ -28,15 +28,7 @@ class Pokemon extends React.Component{
     );   
   }
 }
-// isso deve ficar igual, afinal ainda é essa estrutura que quero ver em cada pokemon
-
-class ButtonNext extends React.Component{
-  render() {
-    return <button onClick={<nextPokemon />}> Click to see next pokemon</button>
-    {/* A cada click, quero que o novo estado seja o próximo pokemon */}
-  }
-}
-// essa class devia estar dentro da Pokedex...?
+// isso deve ficar igual, afinal ainda é essa estrutura que quero ver em cada pokemon e props é imutavel
 
 class Pokedex extends React.Component{
   constructor(props) {
@@ -55,26 +47,31 @@ class Pokedex extends React.Component{
   //n sei como condicionar para que volte para primeiro pokemon quando objeto pokemons acabar
 
   render() {
-    return Allpokemons.map(pokemon => 
-    <Pokemon
-      name={pokemon.name}
-      type={pokemon.type}
-      image={pokemon.image}
-      weight={pokemon.averageWeight.value}
-      unit={pokemon.averageWeight.measurementUnit}
-    />
+    const allPokemons = Allpokemons.map(pokemon => 
+      <Pokemon
+        name={pokemon.name}
+        type={pokemon.type}
+        image={pokemon.image}
+        weight={pokemon.averageWeight.value}
+        unit={pokemon.averageWeight.measurementUnit}
+        />
+    );
+    return (
+      <section>
+      <div>{allPokemons}</div>
+      <button onClick={() => this.nextPokemon}>See next Pokemon</button>
+    </section>
     );
   }
 }
 // o que usar no lugar do map para que apareça apenas um e ir avançando com indice ao clicar o button?
 
 
-// RENDERIZAR TUDO: primeiro pokemon + botao
+// RENDERIZAR TUDO: primeiro pokemon + botao. A priori fica igual.
 function App() {
   return (
     <div className="App">
       <Pokedex />
-      {/* <ButtonNext /> */}
     </div>
   );
 }
