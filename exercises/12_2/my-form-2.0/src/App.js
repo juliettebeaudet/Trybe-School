6be2c134af5d0453df07ff9b6ce890e078eea992
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-18 15:51:10 -0300

    ongoing work on form

--- exercises/12_2/my-form-2.0/src/App.js
+++ exercises/12_2/my-form-2.0/src/App.js
@@ -6,20 +6,6 @@ import Form from './Form.js';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Form />
     </div>
   );
