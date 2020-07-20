c8473a2b183f3cb524d6a0729773e4518175d55f
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-20 14:07:27 -0300

    changed first shown item with event target

--- exercises/13_1/ex3-dropdown/src/App.js
+++ exercises/13_1/ex3-dropdown/src/App.js
@@ -4,19 +4,11 @@ import './App.css';
 
 function App() {
 
-  //sugerido no enunciado
-  const content = [
-    {id: 1, item: 'a'},
-    {id: 2, item: 'b'},
-    {id: 3, item: 'c'},
-    {id: 4, item: 'd'},
-  ];
-
   // aqui apenas renderizar no final
   return (
     <div>
      <Dropdown>
-       Here is my dreamy dropdown! (title test)
+       Here is my dreamy dropdown!
        </Dropdown> 
     </div>
   );
