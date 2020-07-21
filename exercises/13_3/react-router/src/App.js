1f3d96c989b8b54b18033a7591f660b99cf3eb7c
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-21 19:53:58 -0300

    Switch ordenado, acho

--- exercises/13_3/react-router/src/App.js
+++ exercises/13_3/react-router/src/App.js
@@ -3,17 +3,20 @@ import React, { Component } from 'react';
 import Home from './Home';
 import About from './About';
 import Users from './Users';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         {/* <Home /> */}
+        <Switch>
+          {/* Dentro do Switch, ordenar do mais especifico ao mais genêrico */}
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/users" render={(props ) => <Users {...props} greetingMessage="Good morning" />} />
-        {/* <Route path="/users/:id" component={Users} /> */}
+        {/* <Route path="/users" render={(props ) => <Users {...props} greetingMessage="Good morning" />} /> */}
+        <Route path="/users/:id" component={Users} />
+        </Switch>
       </BrowserRouter>
     );
   }
