da9ca44b846e829c64d8ed8fbca24e006360a922
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-07-22 10:50:06 -0300

    StrictAccess on progress

--- exercises/13_3/react-router/src/App.js
+++ exercises/13_3/react-router/src/App.js
@@ -14,14 +14,16 @@ class App extends Component {
         <Switch>
           {/* Dentro do Switch, ordenar do mais especifico ao mais genêrico */}
         <Route exact path="/" component={Home} />
-        <Route path="/strict-access/:user" component={StrictAccess} />
         <Route path="/users/:id" component={Users} />
         <Route path="/users" render={(props ) => <Users {...props} greetingMessage="Good morning" />} />
+        <Route path="/strict-access/" render={(props) => <StrictAccess {...props} user= {username:"joao", password:"1234"} /> }/>
         <Route path="/about" component={About} />
         </Switch>
       </BrowserRouter>
     );
   }
 }
+
+// 14. Adicione no componente App uma rota que mapeie o caminho de URL "/strict-access" para o componente StrictAccess. Para poder testar o fluxo de autenticação, passe para a prop user de StrictAccess diferentes valores para as propriedades username e password.
 
 export default App;
--- exercises/13_3/react-router/src/StrictAccess.js
+++ exercises/13_3/react-router/src/StrictAccess.js
@@ -6,45 +6,15 @@
 // redirecionar quem usa sua aplicação para a página principal.
 
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
-class StrictAccess extends React.Component {
-  render () {
-  // user = this.props.user;
-  return (
-<div>
-  <p>test</p>
-  </div>
-  )
-// escrever uma condicional
-}
+function StrictAccess(props) {
+  const user = props.match.params.user;
+  if (user.username === "joao" && user.password === "1234") return <h2>Welcome {user.username}!</h2>
+  
+  return alert("Access denied") (
+    <Redirect to="/" />
+  );
 }
   
 export default StrictAccess;
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Solutions.css';
-
-function Solutions(props) {
-  const solutionId = props.match.params.solutionId;
-
-  if (solutionId) return <h2>{solutionId}</h2>
-
-  return (
-    <div>
-      <h1>Gabaritos</h1>
-      {props.avaiableSolutions.map((solution) => (
-        <div key={solution}>
-          <span>
-            <Link className="solution-link" to={`/solutions/${solution}`}>
-              {solution}
-            </Link>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default Solutions;
