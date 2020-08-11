171e9832166652e73cb40ed16420071b0a59d3e9
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-11 15:39:24 -0300

    example course of using api request and thunk within react and redux

---
+++ exercises/16 Block 16 - Redux/16_4/doguinhos-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDog } from './store';
+
+function App({ isFetching, src, fetchDog }) {
+  return (
+    isFetching ? <p>Loading</p>
+      : (
+        <div style={{ width: 500 }}>
+          <button
+            style={{ display: 'block' }}
+            onClick={fetchDog}
+            type="button"
+          >
+            Novo Doguinho
+          </button>
+          <img style={{ width: '100%' }} src={src} alt="dog" />
+        </div>
+      )
+  );
+}
+
+const mapStateToProps = (state) => ({
+  src: state.imagePath,
+  isFetching: state.isFetching});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDog: () => dispatch(fetchDog())});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ exercises/16 Block 16 - Redux/16_4/doguinhos-app/src/store/index.js
@@ -0,0 +1,53 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const GET_IMAGE = 'GET_IMAGE';
+const REQUEST_IMAGE = 'REQUEST_IMAGE';
+const FAILED_REQUEST = 'FAILED_REQUEST';
+
+function getImage(json) {
+  return { type: GET_IMAGE, payload: json.message };
+}
+
+function requestDog() {
+  return { type: REQUEST_IMAGE };
+}
+
+function failedRequest(error) {
+  return { type: FAILED_REQUEST, payload: error };
+}
+
+export function fetchDog() {
+  return (dispatch) => {
+    dispatch(requestDog());
+    return fetch('https://dog.ceo/api/breeds/image/random')
+      .then((r) => r.json()
+        .then(
+          (json) => dispatch(getImage(json)),
+          (error) => dispatch(failedRequest(error)),
+        ));
+  };
+}
+
+const initialState = {
+  isFetching: false,
+  imagePath: '',
+  error: '',
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_IMAGE:
+      return { ...state, isFetching: true };
+    case GET_IMAGE:
+      return { ...state, imagePath: action.payload, isFetching: false };
+    case FAILED_REQUEST:
+      return { ...state, error: action.payload, isFetching: false };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+export default store;
