ef017bb70cf5402519db2d35c10e5fa058d355a3
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-22 15:41:22 -0300

    taking away movie card 3 projects

--- exercises/13_4 Project Movie Cards CRUD/src/pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class NotFound extends Component {
-  render() {
-    return <div data-testid="404-error">Página não encontrada</div>;
-  }
-}
-
-export default NotFound;
